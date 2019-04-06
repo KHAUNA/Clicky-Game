68340cb184cbb9af550faf40300a51a08e150583
KHAUNA <jtwinturbo@gmail.com>
2019-04-06 11:40:57 -0400

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ImageContainer from "./component/ImageContainer"
 import './App.css';
 
+
 class App extends Component {
     render() {
     return (
--- src/component/ImageContainer.js
+++ src/component/ImageContainer.js
@@ -3,11 +3,13 @@ import Header from "./Header"
 import ImageSquare from "./ImageSquare"
 import people from "../people.json"
 
+// var Shuffle = require('react-shuffle')
+
 
 class ImageContainer extends Component {
 state = {
   people
-}
+};
   // saveBaseState = () => {
   //   this.baseState = this.state
   // }
@@ -17,18 +19,28 @@ state = {
   //need condition to reset state
 
   removeImage = id => {
- 
     const people = this.state.people.filter(element => element.id !== id)
     this.setState({people});
-
   }
 
-
+  shuffleArry = people => {
+    let rand, arry1
+    for (let i = people.length; i > 0; i--) {
+      rand = Math.floor(Math.random() * i + 1)
+      arry1 = people[i]
+      people[i] = people[rand]
+      people[rand] = arry1
+    } return people
+    
+    
+  }
 
   render() {
     return (
       <div>
+        
         <Header />
+        {/* {this.shuffleArry(this.state.people)} */}
         {this.state.people.map(person => (
           <ImageSquare 
             remove = {this.removeImage}
